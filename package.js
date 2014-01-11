b225b29a127f083f3dc436f60479b16e7e43e757
Marek Piasecki <marek.piasecki@netguru.pl>
2014-01-11 21:15:58 +0100

    deleted too much

---
+++ package.js
@@ -0,0 +1,14 @@
+Package.describe({
+  summary: "Models for Meteor"
+});
+
+var both = ['client', 'server'];
+
+Package.on_use(function (api) {
+  api.use('coffeescript', both);
+  api.add_files('lib/model.coffee', both);
+});
+
+Package.on_test(function (api) {
+  api.add_files('tests/model_tests.coffee', both);
+});
